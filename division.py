ac2f6d52811b5e7c55e58ea244138bb530e603f1
eduynza <63824219+eduynza@users.noreply.github.com>
2020-04-16 19:55:49 -0500

    Add files via upload

---
+++ division.py
@@ -0,0 +1,6 @@
+numero1 = float(input("introduzca el primer número"))
+numero2 = float(input("introduzca el segundo número"))
+if numero2  == 0:
+	print ("El divisor no puede ser cero")
+else:
+	print (numero1/numero2)
